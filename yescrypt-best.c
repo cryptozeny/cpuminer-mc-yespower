ad9d9e8560b6563d393f61a34f00b4b1b488ff0e
prime <prime@kmc.gr.jp>
2018-01-05 06:54:14 +0900

    add ARM NEON version for yescrypt

--- yescrypt-best.c
+++ yescrypt-best.c
@@ -1,5 +1,7 @@
-#ifdef __SSE2__
-#include "yescrypt-simd.c"
+#ifdef __ARM_NEON__
+#include "yescrypt-neon.c"
+#elif defined __SSE2__
+#include "yescrypt-sse.c"
 #else
 #include "yescrypt-opt.c"
 #endif
