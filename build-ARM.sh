510baf994f23351c47225931f007734b749cefe1
cryptozeny <cryptozeny@gmail.com>
2018-07-25 03:07:55 +0900

    create: build-arm.sh

---
+++ build-ARM.sh
@@ -0,0 +1,5 @@
+make clean ; \
+./autogen.sh && \
+./nomacro.pl && \
+./configure CFLAGS="-Ofast -mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -ffast-math" CXXFLAGS="-Ofast -mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard -ffast-math" && \
+make
