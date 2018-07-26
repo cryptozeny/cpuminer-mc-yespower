60b7da98177e8cb7a6f0f7eb3750666b7f5b5890
cryptozeny <cryptozeny@gmail.com>
2018-07-26 21:50:18 +0900

    create: solardiz CFLAGS -O2 -fomit-frame-pointer -msse2

--- build-solardiz.sh
+++ build-solardiz.sh
@@ -2,5 +2,12 @@
 make clean ; \
 ./autogen.sh && \
 ./nomacro.pl && \
+
+# solardiz 16 hours ago  •
+# I recommend -O2 -fomit-frame-pointer instead of -O3, 
+# although the difference is minor
+# (and expected to be seen mostly on 32-bit x86 builds,
+# where you'd also want to add -msse2).
+
 ./configure CFLAGS="-O2 -fomit-frame-pointer -msse2" && \
 make
